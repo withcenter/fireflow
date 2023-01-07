4ff02f0691b5ef958cb6f22a7d84422486f8135c
JaeHo Song <thruthesky@gmail.com>
2023-01-07 15:56:45 +0900

    rules

--- lib/src/chat/chat_room.model.dart
+++ lib/src/chat/chat_room.model.dart
@@ -12,6 +12,7 @@ class ChatRoomModel {
   List<DocumentReference>? moderatorUserDocumentReferences;
   List<DocumentReference> unsubscribedUserDocumentReferences;
   bool isGroupChat;
+  bool isOpenChat;
 
   ChatRoomModel({
     required this.userDocumentReferences,
@@ -23,6 +24,7 @@ class ChatRoomModel {
     required this.moderatorUserDocumentReferences,
     required this.unsubscribedUserDocumentReferences,
     required this.isGroupChat,
+    required this.isOpenChat,
   });
 
   factory ChatRoomModel.fromSnapshot(DocumentSnapshot snapshot) {
@@ -37,8 +39,7 @@ class ChatRoomModel {
     String? id,
   }) {
     return ChatRoomModel(
-      userDocumentReferences:
-          List<DocumentReference>.from(json['userDocumentReferences'] ?? []),
+      userDocumentReferences: List<DocumentReference>.from(json['userDocumentReferences'] ?? []),
       lastMessage: json['lastMessage'] ?? '',
       lastMessageSentAt: json['lastMessageSentAt'] ?? Timestamp.now(),
       lastMessageSeenBy: List<DocumentReference>.from(
@@ -46,17 +47,18 @@ class ChatRoomModel {
       ),
       lastMessageSentBy: json['lastMessageSentBy'],
       title: json['title'],
-      moderatorUserDocumentReferences: List<DocumentReference>.from(
-          json['moderatorUserDocumentReferences'] ?? []),
-      unsubscribedUserDocumentReferences: List<DocumentReference>.from(
-          json['unsubscribedUserDocumentReferences'] ?? []),
+      moderatorUserDocumentReferences:
+          List<DocumentReference>.from(json['moderatorUserDocumentReferences'] ?? []),
+      unsubscribedUserDocumentReferences:
+          List<DocumentReference>.from(json['unsubscribedUserDocumentReferences'] ?? []),
       isGroupChat: json['isGroupChat'] ?? false,
+      isOpenChat: json['isOpenChat'] ?? false,
     );
   }
 
   // create "toString()" method that returns a string of the object of this class
   @override
   String toString() {
-    return 'ChatRoomModel{ userDocumentReferences: $userDocumentReferences, lastMessage: $lastMessage, lastMessageSentAt: $lastMessageSentAt, lastMessageSeenBy: $lastMessageSeenBy, lastMessageSentBy: $lastMessageSentBy, title: $title, moderatorUserDocumentReferences: $moderatorUserDocumentReferences, unsubscribedUserDocumentReferences: $unsubscribedUserDocumentReferences, isGroupChat: $isGroupChat }';
+    return 'ChatRoomModel{ userDocumentReferences: $userDocumentReferences, lastMessage: $lastMessage, lastMessageSentAt: $lastMessageSentAt, lastMessageSeenBy: $lastMessageSeenBy, lastMessageSentBy: $lastMessageSentBy, title: $title, moderatorUserDocumentReferences: $moderatorUserDocumentReferences, unsubscribedUserDocumentReferences: $unsubscribedUserDocumentReferences, isGroupChat: $isGroupChat, isOpenChat: $isOpenChat }';
   }
 }
