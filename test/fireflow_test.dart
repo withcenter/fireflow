8a037cc884743633680ccbfd776fefbdbe067ccf
JaeHo Song <thruthesky@gmail.com>
2023-01-27 21:30:54 +0800

    doc

--- test/fireflow_test.dart
+++ test/fireflow_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fireflow/fireflow.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
 void main() {
   // test('adds one to input values', () {
@@ -11,6 +12,7 @@ void main() {
   // });
 
   test('AppService::addOne', () {
+    final fakeDb = FakeFirebaseFirestore();
     final appService = AppService.instance;
     expect(appService.runtimeType, AppService);
   });
