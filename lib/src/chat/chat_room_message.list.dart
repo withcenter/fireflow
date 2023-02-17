2641431387bedeec8df62f0ece9f4b15cdffe00d
JaeHo Song <thruthesky@gmail.com>
2023-02-18 02:19:45 +0800

    update url preivew

--- lib/src/chat/chat_room_message.list.dart
+++ lib/src/chat/chat_room_message.list.dart
@@ -20,8 +20,10 @@ class ChatRoomMessageList extends StatefulWidget {
     required this.onEmpty,
     this.onProtocolMessage,
   })  : assert(
-            (otherUserPublicDataDocument != null && chatRoomDocumentReference == null) ||
-                (otherUserPublicDataDocument == null && chatRoomDocumentReference != null),
+            (otherUserPublicDataDocument != null &&
+                    chatRoomDocumentReference == null) ||
+                (otherUserPublicDataDocument == null &&
+                    chatRoomDocumentReference != null),
             "You must set only one of otherUserPublicDataDocument or chatRoomDocumentReference."),
         super(key: key);
 
@@ -65,7 +67,8 @@ class _ChatRoomMessageListState extends State<ChatRoomMessageList> {
     if (isGroupChat) {
       return widget.chatRoomDocumentReference!;
     } else {
-      return ChatService.instance.room(([my.uid, widget.otherUserPublicDataDocument!.id]..sort()).join('-'));
+      return ChatService.instance.room(
+          ([my.uid, widget.otherUserPublicDataDocument!.id]..sort()).join('-'));
     }
   }
 
@@ -97,7 +100,8 @@ class _ChatRoomMessageListState extends State<ChatRoomMessageList> {
     } else {
       // For the open group chat, any user can join the chat room.
       room = ChatRoomModel.fromSnapshot(await chatRoomRef.get());
-      if (room.userDocumentReferences.contains(myReference) == false && room.isOpenChat == true) {
+      if (room.userDocumentReferences.contains(myReference) == false &&
+          room.isOpenChat == true) {
         await chatRoomRef.update({
           'userDocumentReferences': FieldValue.arrayUnion([myReference]),
         });
@@ -151,7 +155,8 @@ class _ChatRoomMessageListState extends State<ChatRoomMessageList> {
       reverse: true,
       // item builder type is compulsory.
       itemBuilder: (context, documentSnapshots, index) {
-        final message = ChatRoomMessageModel.fromSnapshot(documentSnapshots[index]);
+        final message =
+            ChatRoomMessageModel.fromSnapshot(documentSnapshots[index]);
 
         if (message.isProtocol) {
           if (widget.onProtocolMessage != null) {
